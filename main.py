064e62e0b1be4d2ce88901285523f3fe077b1661
Tomoka64 <y.tomoka64@gmail.com>
2018-09-15 16:37:30 +0900

    fix

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from random import randint
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (50, 205, 50)
-surfaceWidth = 800
-surfaceHeight = 500
+screenWidth = 800
+screenHeight = 500
 img = pygame.image.load('birdy.png')
 
 image_height = 80
 image_width = 86
 
 pygame.init()
-screen = pygame.display.set_mode((surfaceWidth, surfaceHeight))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Flappy Bird!")
 clock = pygame.time.Clock()
 background_image = pygame.image.load("sky.jpeg").convert_alpha()
@@ -22,7 +22,13 @@ def flappy_bird(x, y, image):
     screen.blit(img, (x, y))
 
 def background(x, y, image):
-    screen.blit(background_image, (surfaceHeight, surfaceWidth))
+    screen.blit(background_image, (screenHeight, screenWidth))
+
+def getColor():
+    x = randint(0, 255)
+    y = randint(0, 255)
+    z = randint(0, 255)
+    return (x, y, z)
 
 def score(count):
     font = pygame.font.Font('freesansbold.ttf', 20)
@@ -41,11 +47,11 @@ def sendMessage(text):
     lText = pygame.font.Font('freesansbold.ttf', 150)
 
     titleTextSurf, titleTextRect = makeTextObjs(text, lText, white)
-    titleTextRect.center = surfaceWidth / 2, surfaceHeight / 2
+    titleTextRect.center = screenWidth / 2, screenHeight / 2
     screen.blit(titleTextSurf, titleTextRect)
 
     typTextSurf, typTextRect = makeTextObjs('Press any key to continue!', sText, white)
-    typTextRect.center = surfaceWidth / 2, ((surfaceHeight / 2) + 100)
+    typTextRect.center = screenWidth / 2, ((screenHeight / 2) + 100)
     screen.blit(typTextSurf, typTextRect)
 
     pygame.display.update()
@@ -67,9 +73,9 @@ def replay_or_quit():
         return event.key
     return None
 
-def blocks(x, y, wid, height, gap):
-    pygame.draw.rect(screen, green, [x, y, wid, height])
-    pygame.draw.rect(screen, green, [x, y + height + gap, wid, surfaceHeight])
+def blocks(x, y, wid, height, gap, color):
+    pygame.draw.rect(screen, color, [x, y, wid, height])
+    pygame.draw.rect(screen, color, [x, y + height + gap, wid, screenHeight])
 
 def main():
     x = 5
@@ -77,15 +83,16 @@ def main():
     y_move = 1
     game_over = False
 
-    x_block = surfaceWidth
+    x_block = screenWidth
     y_block = 0
 
     block_width = 75
-    block_height = randint(0, surfaceHeight / 2)
+    block_height = randint(0, screenHeight / 2)
     gap = image_height * 3
-    block_move = 10
+    block_move = 6
 
     current_score = 0
+    color = getColor()
 
     while not game_over:
         for event in pygame.event.get():
@@ -98,22 +105,23 @@ def main():
                 if event.key == pygame.K_UP:
                     y_move = 5
         y += y_move
-
         screen.fill(black)
         score(current_score)
         # pygame.transform.scale(background_image, (1280, 1024))
         # screen.blit(background_image, (0,0))
         flappy_bird(x, y, img)
-
-        blocks(x_block, y_block, block_width, block_height, gap)
+        blocks(x_block, y_block, block_width, block_height, gap, color)
         x_block -= block_move
 
         roof = 0
-        if y > surfaceHeight - 80 or y < roof:
+        if y > screenHeight - 80 or y < roof:
             gameOver()
         if x_block < (-1 * block_width):
-            x_block = surfaceWidth
-            block_height = randint(0, surfaceHeight / 2)
+            x_block = screenWidth
+            color = getColor()
+            current_score += 1
+            block_move += 1
+            block_height = randint(0, screenHeight / 2)
 
         if image_width + x > x_block:
             if x < x_block + block_width:
@@ -125,11 +133,8 @@ def main():
                 if x < block_width + x_block:
                     gameOver()
 
-        if x < x_block and x > x_block - block_move:
-            current_score += 1
-
         pygame.display.update()
-        clock.tick(260)
+        clock.tick(60)
 
 main()
 pygame.quit()
